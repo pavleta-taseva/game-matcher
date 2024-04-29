588483206a6fbb865e86e13c75cfed8001057bd7
PavletaT <pavleta.taseva@evalue.cz>
2024-04-29 15:22:50 +0300

    Created SSR page components

---
+++ game-match/app/[...id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const GameDetails = () => {
+    return (
+        <div>Game Details</div>
+    )
+}
+
+export default GameDetails;
---
+++ game-match/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignUpPage = () => {
+    return (
+        <div>Sign up</div>
+    )
+}
+
+export default SignUpPage;
