c855889179617cd600faf052e3ee9b596af615eb
PavletaT <pavleta.taseva@evalue.cz>
2024-05-04 09:18:23 +0300

    setup

--- game-match/app/[...id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const GameDetails = () => {
-    return (
-        <div>Game Details</div>
-    )
-}
-
-export default GameDetails;
--- game-match/app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SignUpPage = () => {
-    return (
-        <div>Sign up</div>
-    )
-}
-
-export default SignUpPage;
