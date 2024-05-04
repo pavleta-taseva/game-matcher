8573ad7be27817419ecce2f74e1d7fb4eb7064b0
PavletaT <pavleta.taseva@evalue.cz>
2024-05-04 09:20:30 +0300

    Update file structure

---
+++ src/components/navlinks.tsx
@@ -0,0 +1,45 @@
+import { IoGameControllerOutline } from "react-icons/io5";
+import { GiEgyptianProfile } from "react-icons/gi";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
+import { RiLoginCircleLine } from "react-icons/ri";
+import { IoMdLogOut } from "react-icons/io";
+import { IoMdPersonAdd } from "react-icons/io";
+
+export const navLinks = [
+    {
+        path: '/',
+        name: 'Home',
+        icon: <IoGameControllerOutline style={{ marginRight: 8 }} />,
+        isLoggedIn: false
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        icon: <GiEgyptianProfile style={{ marginRight: 8 }} />,
+        isLoggedIn: true
+    },
+    {
+        path: '/register',
+        name: 'Sign up',
+        icon: <IoMdPersonAdd style={{ marginRight: 8 }} />,
+        isLoggedIn: false
+    },
+    {
+        path: '/favorites',
+        name: 'Favorites',
+        icon: <MdOutlineFavoriteBorder style={{ marginRight: 8 }} />,
+        isLoggedIn: true
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        icon: <RiLoginCircleLine style={{ marginRight: 8 }} />,
+        isLoggedIn: false
+    },
+    {
+        path: '/logout',
+        name: 'Logout',
+        icon: <IoMdLogOut style={{ marginRight: 8 }} />,
+        isLoggedIn: true
+    }
+]
