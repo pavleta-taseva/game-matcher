6a80cb2467e70f720cf0f502d691336b3df37a39
PavletaT <pavleta.taseva@gmail.com>
2024-08-06 15:47:55 +0300

    Added google profile image or default icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,10 @@
 import React, { JSX } from 'react';
 import { usePathname } from 'next/navigation';
 import { navLinks } from '@/src/utils/navlinks';
-import { signIn, signOut, useSession } from 'next-auth/react';
-import { FaUserCircle } from "react-icons/fa";
+import { signOut, useSession } from 'next-auth/react';
+import { FaCircleUser } from "react-icons/fa6";
 import Link from 'next/link';
 import Image from 'next/image';
-import profileDefault from '@/public/images/user.png';
 
 type NavLink = {
   path: string;
@@ -21,9 +20,13 @@ const Navbar = () => {
   const { data: session } = useSession();
   const profileImage = session?.user?.image;
 
+  const handleLogout = async (linkName: string) => {
+    if (linkName === 'Logout') await signOut();
+  }
+
   return (
-    <div className="flex flex-col py-2 md:p-8 gap-4 md:gap-12 bg-primaryBlack text-3xl w-full h-28 md:h-fit justify-between items-center lg:flex-row sm:text-base">
-      <div className="w-full flex md:justify-start items-center lg:w-1/2">
+    <div className="flex flex-col py-2 md:p-8 gap-4 md:gap-12 bg-primaryBlack text-3xl w-full h-32 md:h-fit justify-between items-center lg:flex-row sm:text-base">
+      <div className="flex md:justify-start items-center w-full lg:w-1/2">
         <Link href={'/'}>
           <div className="text-secondaryBlue flex flex-row gap-2 justify-center items-center text-3xl purple-purse-regular">
             <Image
@@ -36,12 +39,13 @@ const Navbar = () => {
           </div>
         </Link>
       </div>
-      <div className="w-full gap-4 flex justify-center items-center md:justify-start lg:justify-end lg:w-1/2">
+      <div className="flex justify-center items-center w-full h-14 gap-4 md:justify-start lg:justify-end lg:w-1/2">
         {navLinks.map((link: NavLink, index: number) => (
           <div key={index} className="text-2xl lg:text-base">
             {link.session && session && (
               <Link key={index} href={link.path} aria-label={link.name}>
                 <div
+                  onClick={() => handleLogout(link.name)}
                   className={`${pathName === link.path
                     ? 'flex items-center border-b-0 sm:border-b-2 border-primaryLight'
                     : 'flex items-center hover:text-secondaryBlue'
@@ -71,7 +75,23 @@ const Navbar = () => {
             )}
           </div>
         ))}
-        <Image className='h-8 w-8 rounded-full' alt='User Image' src={profileImage || profileDefault} width={40} height={40} />
+        <>
+          {session && session.user &&
+            <div className="group/item relative">
+              <Image
+                alt="User Image"
+                className="rounded-full"
+                src={profileImage || ''}
+                width={30}
+                height={30}
+              />
+              <span className="absolute top-9 right-9 text-center text-sm text-primaryDark bg-primaryLight z-10 hidden w-auto rounded-md bg-gray-900 px-3 py-1 group-hover/item:block whitespace-nowrap">
+                {session?.user?.name}
+              </span>
+            </div>
+          }
+          {!session && <FaCircleUser className='text-2xl md:text-3xl text-primaryBlue' />}
+        </>
       </div>
     </div>
   );
