48b5dd2a15bf154de5679833e4fd8b695d344028
PavletaT <pavleta.taseva@gmail.com>
2024-11-18 08:40:36 +0200

    Update protected routes

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -6,7 +6,7 @@ import { useSession } from 'next-auth/react';
 import { useAuth } from '@/../context/AuthProvider';
 import Spinner from '../ui/Spinner';
 
-const privateRoutes = ['/logout'];
+const privateRoutes = ['logout', 'user', 'favorites'];
 const publicRoutes = ['/login', '/register'];
 
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
@@ -15,10 +15,11 @@ function ProtectedRoute({ children }: { children: React.ReactNode }) {
 
   const router = useRouter();
   const pathName = usePathname();
+  const path = pathName.split('/')[1];
   const [isRedirecting, setIsRedirecting] = useState(status === 'loading');
 
   const isUserLoggedIn = !!session?.user || isLoggedIn || user?.userId;
-  const isPrivateRoute = privateRoutes.includes(pathName);
+  const isPrivateRoute = privateRoutes.some(pathEl => pathEl === path);
   const isPublicRoute = publicRoutes.includes(pathName);
 
   const handleRedirect = () => {
